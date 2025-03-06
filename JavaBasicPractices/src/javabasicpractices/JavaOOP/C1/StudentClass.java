9e269d03e1303c9d0a356f8dc94b7f9db74ad2fc
prothesbarai <aspprothesbarai1619@gmail.com>
2025-03-06 19:20:36 +0600

    v1.2.8.0

--- JavaBasicPractices/src/javabasicpractices/JavaOOP/C1/StudentClass.java
+++ JavaBasicPractices/src/javabasicpractices/JavaOOP/C1/StudentClass.java
@@ -1,10 +1,6 @@
 package javabasicpractices.JavaOOP.C1;
 public class StudentClass {
     
-    private float marks;
-    protected String Names;
-    public int id;
-    public float cgpa;
     
     
     
