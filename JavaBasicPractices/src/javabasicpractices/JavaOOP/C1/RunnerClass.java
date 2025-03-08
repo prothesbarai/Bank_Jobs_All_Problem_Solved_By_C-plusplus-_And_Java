e3ff2140e3e37ecb9e52a59a5b3e0fc3e2a48574
prothesbarai <aspprothesbarai1619@gmail.com>
2025-03-08 10:32:53 +0600

    v1.2.3.4.1.2

--- JavaBasicPractices/src/javabasicpractices/JavaOOP/C1/RunnerClass.java
+++ JavaBasicPractices/src/javabasicpractices/JavaOOP/C1/RunnerClass.java
@@ -2,7 +2,10 @@ package javabasicpractices.JavaOOP.C1;
 public class RunnerClass {
     public static void main(String[] args) {
         
-        StudentClass studentClass = new StudentClass();
+        StudentClass studentClass = new StudentClass("Prothes", 10, 10);
+        studentClass.getData("Asp", 20, 10);
+        studentClass.display();
+        
         
     }
 }
--- JavaBasicPractices/src/javabasicpractices/JavaOOP/C1/StudentClass.java
+++ JavaBasicPractices/src/javabasicpractices/JavaOOP/C1/StudentClass.java
@@ -6,12 +6,22 @@ public class StudentClass {
     public int id;
     public int roll;
     
-    public void getData(){
-        
+    public StudentClass(String names, int id, int roll){
+        this.names = names;
+        this.id = id;
+        this.roll = roll;
+    }
+    
+    public void getData(String names, int id, int roll){
+        this.names = names;
+        this.id = id;
+        this.roll = roll;
     }
     
     public void display(){
-        
+        System.out.println("Name : "+names);
+        System.out.println("Name : "+id);
+        System.out.println("Name : "+roll);
     }
     
     
